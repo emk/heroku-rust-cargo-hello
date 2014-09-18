e5a3bbb1eec2fd51306708b3f0226750ce60ac35
Eric Kidd <git@randomhacks.net>
2014-09-18 12:09:21 -0400

    Add logger to middleware chain
    We also update our Rust and libraries, as usual.

--- src/hello.rs
+++ src/hello.rs
@@ -1,12 +1,14 @@
 extern crate http;
 extern crate iron;
 extern crate router;
+extern crate logger;
 
 use std::os::getenv;
 use std::io::net::ip::{Ipv4Addr, Port};
-use iron::{Iron, Request, Response, IronResult};
+use iron::{Chain, ChainBuilder, Iron, Request, Response, IronResult};
 use iron::status;
 use router::{Router, Params};
+use logger::Logger;
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
@@ -29,8 +31,17 @@ fn get_server_port() -> Port {
 
 /// Configure and run our server.
 fn main() {
+    // Set up our URL router.
     let mut router = Router::new();
     router.get("/", hello);
     router.get("/:name", hello_name);
-    Iron::new(router).listen(Ipv4Addr(0, 0, 0, 0), get_server_port());
+
+    // Create a chain with our router and a logger.
+    let mut chain = ChainBuilder::new(router);
+    let (logger_before, logger_after) = Logger::new(None);
+    chain.link_before(logger_before);
+    chain.link_after(logger_after);
+
+    // Run the server.
+    Iron::new(chain).listen(Ipv4Addr(0, 0, 0, 0), get_server_port());
 }
