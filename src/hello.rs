91571025810fd85a39b87befd44a19da8801616e
Eric Kidd <git@randomhacks.net>
2015-01-16 19:39:01 -0500

    Update to _almost_ compile with latest Rust
    The compiler crashes with an internal error that may be fixed tomorrow.

--- src/hello.rs
+++ src/hello.rs
@@ -1,35 +1,33 @@
+#![allow(unstable)]
+
 extern crate iron;
 extern crate router;
 
+use std::str::FromStr;
 use std::os::getenv;
 use std::io::net::ip::{Ipv4Addr, Port};
-use iron::{Iron, Request, Response, IronResult, Set};
-use iron::response::modifiers::{Status, Body};
+use iron::{Iron, Request, Response, IronResult};
+use router::Router;
 use iron::status;
-use router::{Router, Params};
 
 // Serves a string to the user.  Try accessing "/".
 fn hello(_: &mut Request) -> IronResult<Response> {
-    let resp = Response::new()
-        .set(Status(status::Ok))
-        .set(Body("Hello world!"));
+    let resp = Response::with((status::Ok, "Hello world!"));
     Ok(resp)
 }
 
 // Serves a customized string to the user.  Try accessing "/world".
 fn hello_name(req: &mut Request) -> IronResult<Response> {
-    let params = req.extensions.get::<Router,Params>().unwrap();
+    let params = req.extensions.get::<Router>().unwrap();
     let name = params.find("name").unwrap();
-    let resp = Response::new()
-        .set(Status(status::Ok))
-        .set(Body(format!("Hello, {}!", name)));
+    let resp = Response::with((status::Ok, format!("Hello, {}!", name)));
     Ok(resp)
 }
 
 /// Look up our server port number in PORT, for compatibility with Heroku.
 fn get_server_port() -> Port {
     getenv("PORT")
-        .and_then(|s| from_str::<Port>(s.as_slice()))
+        .and_then(|s| FromStr::from_str(s.as_slice()))
         .unwrap_or(8080)
 }
 
@@ -41,8 +39,5 @@ fn main() {
     router.get("/:name", hello_name);
 
     // Run the server.
-    match Iron::new(router).listen((Ipv4Addr(0, 0, 0, 0), get_server_port())) {
-        Ok(_) => {},
-        Err(ref err) => { println!("error starting server: {}", err); }
-    }
+    Iron::new(router).listen((Ipv4Addr(0, 0, 0, 0), get_server_port())).unwrap();
 }
