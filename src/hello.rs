084e12018c6dba33638678a9cd5028fc1b2c80e7
Eric Kidd <git@randomhacks.net>
2015-05-10 11:09:01 -0400

    Update for latest Rust

--- src/hello.rs
+++ src/hello.rs
@@ -1,13 +1,8 @@
-#![feature(os)]
-#![feature(io)]
-#![feature(core)]
-
 extern crate iron;
 extern crate router;
 
 use std::str::FromStr;
-use std::os::getenv;
-use std::old_io::net::ip::{Ipv4Addr, Port};
+use std::env;
 use iron::{Iron, Request, Response, IronResult};
 use router::Router;
 use iron::status;
@@ -27,10 +22,9 @@ fn hello_name(req: &mut Request) -> IronResult<Response> {
 }
 
 /// Look up our server port number in PORT, for compatibility with Heroku.
-fn get_server_port() -> Port {
-    getenv("PORT")
-        .and_then(|s| FromStr::from_str(&s[]))
-        .unwrap_or(8080)
+fn get_server_port() -> u16 {
+    let port_str = env::var("PORT").unwrap_or(String::new());
+    FromStr::from_str(&port_str).unwrap_or(8080)
 }
 
 /// Configure and run our server.
@@ -41,5 +35,5 @@ fn main() {
     router.get("/:name", hello_name);
 
     // Run the server.
-    Iron::new(router).listen((Ipv4Addr(0, 0, 0, 0), get_server_port())).unwrap();
+    Iron::new(router).http(("0.0.0.0", get_server_port())).unwrap();
 }
