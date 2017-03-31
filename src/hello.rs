ec1dbbbcbf452283120d9862641966f78ec4067f
David Laban <alsuren@gmail.com>
2017-03-31 23:54:14 +0100

    Port to use hyper master (0.11.0-a.0) so I can piss about with tokio futures

--- src/hello.rs
+++ src/hello.rs
@@ -1,38 +1,46 @@
-extern crate iron;
-extern crate router;
-
+#![deny(warnings)]
+extern crate hyper;
+extern crate futures;
+extern crate pretty_env_logger;
+//extern crate num_cpus;
 use std::env;
-use iron::{Iron, Request, Response, IronResult};
-use router::Router;
-use iron::status;
+use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr};
+use futures::future::FutureResult;
 
-// Serves a string to the user.  Try accessing "/".
-fn hello(_: &mut Request) -> IronResult<Response> {
-    let resp = Response::with((status::Ok, "Hello world!"));
-    Ok(resp)
+use hyper::header::{ContentLength, ContentType};
+use hyper::server::{Http, Service, Request, Response};
+
+static PHRASE: &'static [u8] = b"Hello World!";
+
+#[derive(Clone, Copy)]
+struct Hello;
+
+impl Service for Hello {
+    type Request = Request;
+    type Response = Response;
+    type Error = hyper::Error;
+    type Future = FutureResult<Response, hyper::Error>;
+    fn call(&self, _req: Request) -> Self::Future {
+        futures::future::ok(Response::new()
+                                .with_header(ContentLength(PHRASE.len() as u64))
+                                .with_header(ContentType::plaintext())
+                                .with_body(PHRASE))
+    }
 }
-
-// Serves a customized string to the user.  Try accessing "/world".
-fn hello_name(req: &mut Request) -> IronResult<Response> {
-    let params = req.extensions.get::<Router>().unwrap();
-    let name = params.find("name").unwrap();
-    let resp = Response::with((status::Ok, format!("Hello, {}!", name)));
-    Ok(resp)
-}
-
 /// Look up our server port number in PORT, for compatibility with Heroku.
 fn get_server_port() -> u16 {
     let port_str = env::var("PORT").unwrap_or(String::new());
     port_str.parse().unwrap_or(8080)
 }
 
-/// Configure and run our server.
 fn main() {
-    // Set up our URL router.
-    let mut router: Router = Router::new();
-    router.get("/", hello, "index");
-    router.get("/:name", hello_name, "name");
+    pretty_env_logger::init().unwrap();
+    // There has got to be a better way specify an ip address.
+    let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), get_server_port()));
+    let server = Http::new().bind(&addr, || Ok(Hello)).unwrap();
 
-    // Run the server.
-    Iron::new(router).http(("0.0.0.0", get_server_port())).unwrap();
+    println!("Listening on http://{} with 1 thread.",
+             server.local_addr().unwrap());
+
+    server.run().unwrap();
 }
