7a435fc8c67ed16d4b10b94dac59c94a837046a6
David Laban <alsuren@gmail.com>
2018-02-25 13:56:22 +0000

    cargo fmt

--- src/hello.rs
+++ src/hello.rs
@@ -1,27 +1,27 @@
 #![deny(warnings)]
-extern crate hyper;
-extern crate hyper_tls;
-extern crate tokio_core;
 extern crate futures;
 extern crate futures_cpupool;
+extern crate hyper;
+extern crate hyper_tls;
 extern crate pretty_env_logger;
 extern crate r2d2;
 extern crate r2d2_redis;
 extern crate redis;
+extern crate rmessenger;
 #[macro_use]
 extern crate serde_derive;
 extern crate serde_json;
-extern crate rmessenger;
+extern crate tokio_core;
 
 use std::env;
-use std::net::{SocketAddr, SocketAddrV4, Ipv4Addr};
+use std::net::{Ipv4Addr, SocketAddr, SocketAddrV4};
 use futures::{Future, Stream};
 use futures_cpupool::CpuPool;
 
 use hyper::{Get, Post, StatusCode};
 use hyper::StatusCode::InternalServerError;
 use hyper::header::ContentLength;
-use hyper::server::{Http, Service, Request, Response};
+use hyper::server::{Http, Request, Response, Service};
 
 use r2d2_redis::RedisConnectionManager;
 use redis::Commands;
@@ -29,7 +29,6 @@ use rmessenger::bot::Bot;
 
 use tokio_core::net::TcpListener;
 use tokio_core::reactor::{Core, Handle};
-
 
 type MessengerFuture = Box<Future<Item = Response, Error = hyper::Error>>;
 /*
@@ -142,10 +141,10 @@ impl Echo {
                     .unwrap_or("You're the first!".to_string());
                 let ret: String = conn.set("last_response", query)
                     .unwrap_or("DB ERROR".to_string());
-                let body = format!("Last person said: {} You said: {}. Got back: {}",
-                                   last,
-                                   query,
-                                   ret);
+                let body = format!(
+                    "Last person said: {} You said: {}. Got back: {}",
+                    last, query, ret
+                );
                 let res = Response::new()
                     .with_header(ContentLength((body.len() as u64)))
                     .with_body(body);
@@ -178,49 +177,47 @@ impl Echo {
                 return Box::new(futures::future::ok(res));
             }
             None => {
-                let msg = format!("Incorrect webhook_verify_token or No hub.challenge in {}",
-                                  req.uri().as_ref());
+                let msg = format!(
+                    "Incorrect webhook_verify_token or No hub.challenge in {}",
+                    req.uri().as_ref()
+                );
                 return Box::new(futures::future::err(make_error(msg)));
             }
         }
-
     }
 
     fn handle_webhook_post(&self, req: Request) -> MessengerFuture {
         let bot = self.bot.clone();
-        let body_fut = req.body()
-            .fold(Vec::new(), |mut acc, chunk| {
-                acc.extend_from_slice(&chunk);
-                Ok::<_, hyper::Error>(acc)
-            });
-        let response_fut =
-            body_fut.and_then(move |body| {
-                let json: serde_json::Value = serde_json::from_slice(&body).unwrap_or_default();
-                println!("got webhook: {}", json.to_string());
-                println!("got webhook: {:?}", json);
-                let typed_json: WebhookPayload = serde_json::from_slice(&body).unwrap_or_default();
-                println!("got typed: {:?}", typed_json);
+        let body_fut = req.body().fold(Vec::new(), |mut acc, chunk| {
+            acc.extend_from_slice(&chunk);
+            Ok::<_, hyper::Error>(acc)
+        });
+        let response_fut = body_fut.and_then(move |body| {
+            let json: serde_json::Value = serde_json::from_slice(&body).unwrap_or_default();
+            println!("got webhook: {}", json.to_string());
+            println!("got webhook: {:?}", json);
+            let typed_json: WebhookPayload = serde_json::from_slice(&body).unwrap_or_default();
+            println!("got typed: {:?}", typed_json);
 
-
-                let mut message_futures = Vec::new();
-                for entry in &typed_json.entry {
-                    for message in &entry.messaging {
-                        let text = &message.message.text;
-                        let sender = &message.sender.id;
-                        message_futures.push(bot.send_text_message(sender, text));
-                    }
+            let mut message_futures = Vec::new();
+            for entry in &typed_json.entry {
+                for message in &entry.messaging {
+                    let text = &message.message.text;
+                    let sender = &message.sender.id;
+                    message_futures.push(bot.send_text_message(sender, text));
                 }
-                let joined_futures = futures::future::join_all(message_futures);
+            }
+            let joined_futures = futures::future::join_all(message_futures);
 
-                let response_future = joined_futures.and_then(move |v| {
-                    println!("message sending done: {:?}", v);
+            let response_future = joined_futures.and_then(move |v| {
+                println!("message sending done: {:?}", v);
 
-                    let mut res = Response::new();
-                    res = res.with_body(serde_json::to_string(&typed_json).unwrap_or_default());
-                    Ok(res)
-                });
-                response_future
+                let mut res = Response::new();
+                res = res.with_body(serde_json::to_string(&typed_json).unwrap_or_default());
+                Ok(res)
             });
+            response_future
+        });
         Box::new(response_fut)
     }
 }
@@ -237,7 +234,9 @@ impl Service for Echo {
             (&Post, "/echo") => self.handle_post(req),
             (&Get, "/webhook") => self.handle_webhook_verification(req),
             (&Post, "/webhook") => self.handle_webhook_post(req),
-            _ => Box::new(futures::future::ok(Response::new().with_status(StatusCode::NotFound))),
+            _ => Box::new(futures::future::ok(
+                Response::new().with_status(StatusCode::NotFound),
+            )),
         };
 
         let resp = resp_fut.or_else(|err| {
@@ -278,27 +277,29 @@ fn get_bot(handle: Handle) -> Bot {
     let access_token = env::var("ACCESS_TOKEN").unwrap_or(String::new());
     let app_secret = env::var("APP_SECRET").unwrap_or(String::new());
     let webhook_verify_token = env::var("WEBHOOK_VERIFY_TOKEN").unwrap_or(String::new());
-    Bot::new(get_http_client(handle),
-             &access_token,
-             &app_secret,
-             &webhook_verify_token)
+    Bot::new(
+        get_http_client(handle),
+        &access_token,
+        &app_secret,
+        &webhook_verify_token,
+    )
 }
 
 fn main() {
     pretty_env_logger::init().unwrap();
     // There has got to be a better way specify an ip address.
-    let addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::new(0, 0, 0, 0), get_server_port()));
+    let addr = SocketAddr::V4(SocketAddrV4::new(
+        Ipv4Addr::new(0, 0, 0, 0),
+        get_server_port(),
+    ));
 
     let mut core = Core::new().unwrap();
     let handle: Handle = core.handle();
     let listener = TcpListener::bind(&addr, &handle).unwrap();
     let protocol = Http::new();
     let service = Echo::new(&handle);
-    core.run(listener
-                 .incoming()
-                 .for_each(|(socket, addr)| {
-                               protocol.bind_connection(&handle, socket, addr, service.clone());
-                               Ok(())
-                           }))
-        .unwrap()
+    core.run(listener.incoming().for_each(|(socket, addr)| {
+        protocol.bind_connection(&handle, socket, addr, service.clone());
+        Ok(())
+    })).unwrap()
 }
